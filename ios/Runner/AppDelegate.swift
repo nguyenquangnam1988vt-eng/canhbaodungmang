1296ed72215b269d26e2b6e53c30c45fea973ad8
Nguyen Quang Nam <nguyenquangnam1988vt@gmail.com>
2025-11-07 17:39:10 +0700

    Initial commit - Flutter Network Monitor App

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,27 @@
+import UIKit
+import Flutter
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    
+    // Request notification permission
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+    
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  
+  // Enable background processing
+  override func application(
+    _ application: UIApplication,
+    performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+  ) {
+    completionHandler(.newData)
+  }
+}
